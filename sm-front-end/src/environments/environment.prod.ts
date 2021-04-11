8e9d4fa4a0f1e36c46a26be5121695377da3fdc1
Arturo Cordoba <arturocv16@gmail.com>
2021-04-11 15:23:37 -0600

    Prod server url

--- sm-front-end/src/environments/environment.prod.ts
+++ sm-front-end/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  restapiUrl: 'http://192.168.1.200:9082',
-  webSocketURL: 'ws://192.168.1.200:9081',
+  restapiUrl: 'http://10.0.0.40:9082',
+  webSocketURL: 'ws://10.0.0.40:9081',
 };
